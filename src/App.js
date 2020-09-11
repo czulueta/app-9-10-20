16601aa5048fba0f77bb1b371c57a0b1eb33b7ba
czulueta <https://github.com/czulueta/weed.git>
2020-09-10 21:56:00 -0600

    adding to new app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Link, Switch, Route} from "react-router-dom"
+import home from "/home"
+import products from "/products"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to="/">home</Link>
+      <Link to="/products">products</Link>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function products(){
+    return(
+        <div>
+            <h1>products page</h1>
+        </div>
+    )
+}
+export default products
